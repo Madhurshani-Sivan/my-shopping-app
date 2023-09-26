393cc31a5b92c36cb9e7582eb16a3ba3dbfd7e46
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-09-26 08:31:39 +0530

    added route to home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import HomePage from "./components/modules/home/HomePage";
 import store from "./store/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
-      <Header />
-      <HomePage />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </Provider>
   );
 }
