ebfd24d3b91931ca1b63bc250256f738d4a04c9f
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-09-27 09:46:13 +0530

    added counter

--- src/components/modules/product/includes/includes/productDescription/ProductDescription.tsx
+++ src/components/modules/product/includes/includes/productDescription/ProductDescription.tsx
@@ -1,10 +1,12 @@
 import { Button } from "antd";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { ProductCardProps } from "../../../../../../types/props";
 import StarRating from "./includes/StarRating";
 import styles from "./ProductDescription.module.css";
 
 const ProductDescription: FC<ProductCardProps> = ({ product }) => {
+  const [count, setCount] = useState(1);
+
   return (
     <div className={styles.container}>
       <div>
@@ -42,10 +44,10 @@ const ProductDescription: FC<ProductCardProps> = ({ product }) => {
             <h4>${product.price}</h4>
           </div>
         </div>
-        <div>
-          <Button>+</Button>
-          <Button>1</Button>
-          <Button>-</Button>
+        <div className={styles.counter}>
+          <Button onClick={() => setCount(count + 1)}>+</Button>
+          <Button>{count}</Button>
+          <Button onClick={() => setCount(count - 1)}>-</Button>
         </div>
         <div>
           <Button>Buy Now</Button>
