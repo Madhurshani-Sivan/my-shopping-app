56766f37942b7365ef7873404eb7af128adf36df
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-10-02 08:50:30 +0530

    added cart reducer

--- src/store/RootState.ts
+++ src/store/RootState.ts
@@ -2,10 +2,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 
 import categoriesReducer from "./slices/categoriesReducer";
 import productReducer from "./slices/productReducer";
+import cartReducer from "./slices/cartReducer";
 
 const rootReducer = combineReducers({
   products: productReducer,
   categories: categoriesReducer,
+  cart: cartReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/store/slices/cartReducer.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { CartState } from "../../types/cart";
+
+const initialState: CartState = { items: [] };
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      if (existingItem) {
+        existingItem.quantity += newItem.quantity;
+      } else {
+        state.items.push(newItem);
+      }
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
